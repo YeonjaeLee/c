4b8c75c39dc3e00c3be9c9bcaa425b48e22d1cf8
YeonJae Lee(academy) <yeonjae115@gmail.com>
2017-03-29 17:10:56 +0900

    Chapter07: 반복실행을 명령하는 반복문 while, do~while, for

--- Basic/Chapter07/01_SimpleWhile.c
+++ Basic/Chapter07/01_SimpleWhile.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main1(void)
 {
 	int num = 0;
 
---
+++ Basic/Chapter07/03_TwoToNine.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main3(void)
+{
+	int cur = 2;
+	int is = 0;
+
+	while (cur < 10)
+	{
+		is = 1;
+		while (is < 10)
+		{
+			printf("%d x %d = %d \n", cur, is, cur * is);
+			is++;
+		}
+		cur++;
+		printf("\n");
+	}
+
+	return 0;
+
+}
